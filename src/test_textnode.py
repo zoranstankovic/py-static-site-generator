72ae68ef6744eed0e9064b6d2c65be3232764164
zoranstankovic <6569042+zoranstankovic@users.noreply.github.com>
2025-03-18 08:41:33 +0100

    Separate unit tests into multiple test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,15 +9,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
-    def test_not_eq(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertNotEqual(node, node2)
-
+    def test_not_eq_text(self):
         node = TextNode("This is totally different text node", TextType.ITALIC)
         node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
+    def test_not_eq_type(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
         node = TextNode("This is a url node", TextType.LINK, "http://localhost:4000")
         node2 = TextNode("This is a url node", TextType.LINK, "https://localhost:4000")
         self.assertNotEqual(node, node2)
